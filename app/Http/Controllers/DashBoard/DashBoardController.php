96320fcb1d2942f8f1d86ece34b080b67a0cc6be
Mariam Akter <mariamsimu004@gmail.com>
2023-04-09 13:48:59 +0600

    pie, dought chart added

--- app/Http/Controllers/DashBoard/DashBoardController.php
+++ app/Http/Controllers/DashBoard/DashBoardController.php
@@ -24,6 +24,7 @@ class DashBoardController extends Controller
         $classes=Classes::count();
         $subject=Subject::count();
         return view('welcome',compact('student','teacher','classes','subject'));
+
     }
 
     /**
